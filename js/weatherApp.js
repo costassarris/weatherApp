16d46f30f5bc769724b23a279be4ccbd311d83a2
costassarris <costassarris@gmail.com>
2015-03-05 14:12:08 +0000

    can select city from drop down menu

--- js/weatherApp.js
+++ js/weatherApp.js
@@ -1,22 +1,31 @@
+$(document).ready(function() {
+
+  $.ajax({
+    url: "http://weather-api.herokuapp.com/cities",
+    type: "GET",
+    dataType: "jsonp",
+    success: function(response) {
+      $.each(response.cities, function(index, city) {
+
+        $('.cities').append(
+
+          $("<option></option>").val(city).html(city.capitalize()+" ⌄")
+
+        );
 
 
-$.ajax({
-  url: "http://weather-api.herokuapp.com/cities",
-  type: "GET",
-  dataType: "jsonp",
-  success: function(response) {
-    $.each(response.cities, function(index, city) {
+      });
+    }
+  });
 
-      $('.cities').append(
+  String.prototype.capitalize = function() {
+      return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+  };
 
-        $("<option></option>").val(city).html(city+"⌄")
-
-      );
-
-
+  $('select').change(function() {
+    $("select option:selected").each(function() {
+      console.log(this);
     });
-  }
+  }).trigger("change");
+
 });
-
-
-
